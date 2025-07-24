b016c7af76fd361fb44222c7b2c5096b8cc7d94a
glacks06(mac) <hchan.frm@gmail.com>
2025-07-25 00:41:05 +0900

    닫기버튼 삭제 -> q로 닫기/ idle, run 두가지 상태 5초 간격으로 전환/ idle 상태는 두가지. 뭐가 나올지는 랜덤

--- constants.py
+++ constants.py
@@ -4,22 +4,19 @@ monitor_width, monitor_height = pyautogui.size()
 
 window_x = 100
 window_y = 100
-window_size_x = 110
-window_size_y = 110
+window_size_x = 100
+window_size_y = 100
 
-close_button_x = 0
-close_button_y = 0
-close_button_size_x = 10
-close_button_size_y = 10
-close_button_text = 'x'
-close_button_text_bg_color = 'red'
-close_button_text_color = 'black'
-close_button_border_radius = '10'
-
-img_x = 10
-img_y = 10
+img_x = 0
+img_y = 0
 img_size_x = 100
 img_size_y = 100
+
+state_control_loop_interval = 5000
+state_cnt = 2 # 상태 개수 (현재: idle, run)(2개)
+
+idle_list = ['./idle1.png', './idle2.png']
+idle_motion_loop_interval = 250
 
 run_motion1_path = './run1.png'
 run_motion2_path = './run2.png'
--- main.py
+++ main.py
@@ -9,47 +9,74 @@ import constants as cons
 
 class TransparentWindow(QWidget):
     def __init__(self):
-        super().__init__()
-
+        super().__init__() 
         self.now_x = cons.window_x
         self.now_y = cons.window_y
-        self.setWindowTitle("배경만 투명, 위젯 불투명")
+        self.setWindowTitle("도로롱")
         self.setGeometry(self.now_x, self.now_y, cons.window_size_x, cons.window_size_y)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setStyleSheet("background-color: rgba(0, 0, 0, 1);")  # 거의 투명한 배경
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setFocus()
 
-        btn = QPushButton(cons.close_button_text, self)
-        btn.setGeometry(cons.close_button_x, cons.close_button_y, cons.close_button_size_x, cons.close_button_size_y)
-        btn.setStyleSheet(f"background: {cons.close_button_text_bg_color}; color: {cons.close_button_text_color}; border-radius: {cons.close_button_border_radius}px;")
-        btn.clicked.connect(self.close)  # 반드시 self.close로 시그널 연결
-
+        self.state = 1 # 0: 제자리, 1: 이동
         self.label = QLabel(self)
-        pixmap = QPixmap(cons.run_motion1_path)
+        pixmap = QPixmap(cons.idle_list[0])
         self.label.setPixmap(pixmap)
         self.label.setScaledContents(True)
         self.label.setGeometry(cons.img_x, cons.img_y, cons.img_size_x, cons.img_size_y)  # 원하는 위치와 크기로 조정
+
+        # 상태 변경 (idle -> run)
+        self.timer_state_controler = QTimer(self)
+        self.timer_state_controler.timeout.connect(self.state_controler)
+        self.timer_state_controler.start(cons.state_control_loop_interval)
+
+        # idle 애니메이션
+        self.img_idle_state = 0
+        self.timer_idle = QTimer(self)
+        self.timer_idle.timeout.connect(self.idle_anim)
 
         # 달리기 애니메이션
         self.img_run_state = 0
         self.img_run_direction = 1
         self.timer_run = QTimer(self)
         self.timer_run.timeout.connect(self.run_anim)
-        self.timer_run.start(cons.run_motion_loop_interval)  # 1000ms=1초
-
 
         # 창 이동
         self.speed = cons.window_move_speed
         self.max_x = cons.monitor_width
         self.max_y = cons.monitor_height
-
-        self.goal_x = random.randint(0, self.max_x)
-        self.goal_x -= self.goal_x % self.speed
-        self.goal_y = random.randint(0, self.max_y)
-        self.goal_y -= self.goal_y % self.speed
+        self.setNewPositionGoal()
 
         self.timer_moving = QTimer(self)
         self.timer_moving.timeout.connect(self.move_window)
-        self.timer_moving.start(cons.window_move_loop_interval)  # 0.5초마다 창 이동
+
+    def all_timer_stop(self):
+        self.timer_idle.stop()
+        self.timer_moving.stop()
+        self.timer_run.stop()
+
+    def state_controler(self):
+        self.all_timer_stop()
+
+        if self.state == 0: # 제자리
+            self.img_idle_state = random.randint(0, len(cons.idle_list)-1)
+            self.timer_idle.start(cons.idle_motion_loop_interval)
+            self.state += 1
+            self.state %= cons.state_cnt
+            
+        elif self.state == 1: # 이동+달리기모션
+            self.setNewPositionGoal()
+            self.timer_run.start(cons.run_motion_loop_interval)
+            self.timer_moving.start(cons.window_move_loop_interval)
+            self.state += 1
+            self.state %= cons.state_cnt
+
+    
+    def idle_anim(self):
+        pixmap = QPixmap(cons.idle_list[self.img_idle_state])
+        self.label.setPixmap(pixmap)
 
     def run_anim(self):
         if self.img_run_state == 0:
@@ -67,6 +94,7 @@ class TransparentWindow(QWidget):
     def move_window(self):
         temp_x = self.now_x
         temp_y = self.now_y
+
         if self.now_x < self.goal_x: 
             self.now_x += self.speed
             self.img_run_direction = -1
@@ -80,13 +108,19 @@ class TransparentWindow(QWidget):
             self.now_y -= self.speed
 
         if temp_x == self.now_x and temp_y == self.now_y: # 목표지점에 도달함
-            self.goal_x = random.randint(0, self.max_x)
-            self.goal_x -= self.goal_x % self.speed
-        if temp_y == self.now_y: # 목표지점에 도달함
-            self.goal_y = random.randint(0, self.max_y)
-            self.goal_y -= self.goal_y % self.speed
+            self.setNewPositionGoal()
         
         self.move(self.now_x, self.now_y)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Q: self.close()
+
+    def setNewPositionGoal(self):
+        self.goal_x = random.randint(0, self.max_x)
+        self.goal_x -= self.goal_x % self.speed
+        self.goal_y = random.randint(0, self.max_y)
+        self.goal_y -= self.goal_y % self.speed
+
 
 
 if __name__ == "__main__":
