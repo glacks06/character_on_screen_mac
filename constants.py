059e512007f2931a48ee62101645f5c8bd0edae9
glacks06(mac) <hchan.frm@gmail.com>
2025-08-16 00:31:39 +0900

    kivy 라이브러리로 교체 작업 완료

--- constants.py
+++ constants.py
@@ -12,17 +12,17 @@ img_y = 0
 img_size_x = 100
 img_size_y = 100
 
-state_control_loop_interval = 5000
+state_control_loop_interval = 5
 state_cnt = 2 # 상태 개수 (현재: idle, run)(2개)
 
-idle_list = ['idle1.png', 'idle2.png']
-idle_motion_loop_interval = 250
+idle_list = ['./idle1.png', './idle2.png']
+idle_motion_loop_interval = 0.25
 
-run_motion1_path = 'run1.png'
-run_motion2_path = 'run2.png'
-run_motion_loop_interval = 250
+run_motion1_path = ['./run1_L.png', './run1_R.png']
+run_motion2_path = ['./run2_L.png', './run2_R.png']
+run_motion_loop_interval = 0.25
 
-window_move_loop_interval = 10
+window_move_loop_interval = 0.01
 window_move_speed = 5
 monitor_width, monitor_height = pyautogui.size()
 monitor_width -= img_size_x
--- main.py
+++ main.py
@@ -1,134 +1,236 @@
-import sys
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
-from PyQt5.QtGui import QPixmap, QTransform
 import random
 import constants as cons
-import os
 
-def resource_path(relative_path):
-    base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
-    return os.path.join(base_path, relative_path)
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
 
 
-class TransparentWindow(QWidget):
-    def __init__(self):
-        super().__init__() 
-        self.now_x = cons.window_x
-        self.now_y = cons.window_y
-        self.setWindowTitle("도로롱")
-        self.setGeometry(self.now_x, self.now_y, cons.window_size_x, cons.window_size_y)
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.setStyleSheet("background-color: rgba(0, 0, 0, 1);")  # 거의 투명한 배경
-        self.setFocusPolicy(Qt.StrongFocus)
-        self.setFocus()
+class CharacterWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.img = Image(source=cons.idle_list[0])
+        self.add_widget(self.img)
 
-        self.state = 1 # 0: 제자리, 1: 이동
-        self.label = QLabel(self)
-        pixmap = QPixmap(resource_path(cons.idle_list[0]))
-        self.label.setPixmap(pixmap)
-        self.label.setScaledContents(True)
-        self.label.setGeometry(cons.img_x, cons.img_y, cons.img_size_x, cons.img_size_y)  # 원하는 위치와 크기로 조정
-
-        # 상태 변경 (idle -> run)
-        self.timer_state_controler = QTimer(self)
-        self.timer_state_controler.timeout.connect(self.state_controler)
-        self.timer_state_controler.start(cons.state_control_loop_interval)
-
-        # idle 애니메이션
+        self.state = 1
         self.img_idle_state = 0
-        self.timer_idle = QTimer(self)
-        self.timer_idle.timeout.connect(self.idle_anim)
-
-        # 달리기 애니메이션
         self.img_run_state = 0
         self.img_run_direction = 1
-        self.timer_run = QTimer(self)
-        self.timer_run.timeout.connect(self.run_anim)
+
+        self.now_x = cons.window_x
+        self.now_y = cons.window_y
 
         # 창 이동
         self.speed = cons.window_move_speed
         self.max_x = cons.monitor_width
         self.max_y = cons.monitor_height
-        self.setNewPositionGoal()
 
-        self.timer_moving = QTimer(self)
-        self.timer_moving.timeout.connect(self.move_window)
-
-    def all_timer_stop(self):
-        self.timer_idle.stop()
-        self.timer_moving.stop()
-        self.timer_run.stop()
-
-    def state_controler(self):
-        self.all_timer_stop()
-
-        if self.state == 0: # 제자리
-            self.img_idle_state = random.randint(0, len(cons.idle_list)-1)
-            self.timer_idle.start(cons.idle_motion_loop_interval)
-            self.state += 1
-            self.state %= cons.state_cnt
-            
-        elif self.state == 1: # 이동+달리기모션
-            self.setNewPositionGoal()
-            self.timer_run.start(cons.run_motion_loop_interval)
-            self.timer_moving.start(cons.window_move_loop_interval)
-            self.state += 1
-            self.state %= cons.state_cnt
-
-    
-    def idle_anim(self):
-        pixmap = QPixmap(resource_path(cons.idle_list[self.img_idle_state]))
-        self.label.setPixmap(pixmap)
-
-    def run_anim(self):
-        if self.img_run_state == 0:
-            pixmap = QPixmap(resource_path(cons.run_motion1_path))
-            self.img_run_state = 1
-        else:
-            pixmap = QPixmap(resource_path(cons.run_motion2_path))
-            self.img_run_state = 0
-
-        if self.img_run_direction == -1: # 좌우 반전
-            transform = QTransform().scale(-1, 1)  # 좌우 반전용 transform 생성
-            pixmap = pixmap.transformed(transform)
-        self.label.setPixmap(pixmap)
-
-    def move_window(self):
-        temp_x = self.now_x
-        temp_y = self.now_y
-
-        if self.now_x < self.goal_x: 
-            self.now_x += self.speed
-            self.img_run_direction = -1
-        elif self.now_x > self.goal_x: 
-            self.now_x -= self.speed
-            self.img_run_direction = 1
-
-        if self.now_y < self.goal_y: 
-            self.now_y += self.speed
-        elif self.now_y > self.goal_y: 
-            self.now_y -= self.speed
-
-        if temp_x == self.now_x and temp_y == self.now_y: # 목표지점에 도달함
-            self.setNewPositionGoal()
-        
-        self.move(self.now_x, self.now_y)
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Q: self.close()
-
-    def setNewPositionGoal(self):
         self.goal_x = random.randint(0, self.max_x)
         self.goal_x -= self.goal_x % self.speed
         self.goal_y = random.randint(0, self.max_y)
         self.goal_y -= self.goal_y % self.speed
 
 
+        Clock.schedule_interval(self.state_controler, cons.state_control_loop_interval)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TransparentWindow()
-    window.show()
-    sys.exit(app.exec_())
+    def all_timer_stop(self):
+        Clock.unschedule(self.move_window)
+        Clock.unschedule(self.idle_anim)
+        Clock.unschedule(self.run_anim)
+
+
+    def state_controler(self, dt):
+        self.all_timer_stop()
+        if self.state == 0:  # 제자리
+            self.img_idle_state = random.randint(0, len(cons.idle_list)-1)
+            Clock.schedule_interval(self.idle_anim, cons.idle_motion_loop_interval)
+            self.state = 1
+        elif self.state == 1:  # 이동+달리기
+            self.setNewPositionGoal()
+            Clock.schedule_interval(self.run_anim, cons.run_motion_loop_interval)
+            Clock.schedule_interval(self.move_window, cons.window_move_loop_interval)
+            self.state = 0
+
+    def move_window(self, dt):
+        temp_x = self.now_x
+        temp_y = self.now_y
+        if self.now_x < self.goal_x:
+            self.now_x += self.speed
+            self.img_run_direction = 1
+        elif self.now_x > self.goal_x:
+            self.now_x -= self.speed
+            self.img_run_direction = -1
+
+        if self.now_y < self.goal_y:
+            self.now_y += self.speed
+        elif self.now_y > self.goal_y:
+            self.now_y -= self.speed
+
+        # 실제 윈도우 창 위치 이동
+        Window.left = self.now_x
+        Window.top = self.now_y
+
+        if temp_x == self.now_x and temp_y == self.now_y:
+            self.setNewPositionGoal()
+
+    def idle_anim(self, dt):
+        self.img.source = cons.idle_list[self.img_idle_state]
+
+    def run_anim(self, dt):
+        # 두 이미지를 번갈아가며 사용
+        # print(self.img_run_direction)
+        if self.img_run_state == 0:
+            self.img.source = cons.run_motion1_path[0] if self.img_run_direction == -1 else cons.run_motion1_path[1]
+            print(cons.run_motion1_path[0] if self.img_run_direction == -1 else cons.run_motion1_path[1])
+            self.img_run_state = 1
+        else:
+            self.img.source = cons.run_motion2_path[0] if self.img_run_direction == -1 else cons.run_motion2_path[1]
+            print(cons.run_motion2_path[0] if self.img_run_direction == -1 else cons.run_motion2_path[1])
+            self.img_run_state = 0
+        
+            
+    def setNewPositionGoal(self):
+        self.goal_x = random.randint(0, self.max_x - cons.window_size_x)
+        self.goal_x -= self.goal_x % self.speed
+        self.goal_y = random.randint(0, self.max_y - cons.window_size_y)
+        self.goal_y -= self.goal_y % self.speed
+
+
+
+class MovingCharacterApp(App):
+    def build(self):
+        Window.size = (cons.window_size_x, cons.window_size_y)
+        Window.left = cons.window_x
+        Window.top = cons.window_y
+        Window.clearcolor = (0, 0, 0, 0)  # 완전 투명 배경
+        Window.borderless = True  # 프레임/테두리 제거
+        return CharacterWidget()
+
+if __name__ == '__main__':
+    MovingCharacterApp().run()
+
+# def resource_path(relative_path):
+#     base_path = os.path.abspath(".")
+#     return os.path.join(base_path, relative_path)
+
+# class CharacterWidget(Widget):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.now_x = cons.window_x
+#         self.now_y = cons.window_y
+#         self.state = 1  # 0: idle, 1: run
+
+#         self.img_idle_state = 0
+#         self.timer_idle_ev = None
+#         self.timer_run_ev = None
+#         self.timer_moving_ev = None
+
+#         self.img_run_state = 0
+#         self.img_run_direction = 1
+
+#         self.speed = cons.window_move_speed
+#         self.max_x = cons.monitor_width
+#         self.max_y = cons.monitor_height
+
+#         # 캐릭터 이미지
+#         self.label = Image(
+#             source=resource_path(cons.idle_list[0]),
+#             size=(cons.img_size_x, cons.img_size_y),
+#             pos=(cons.img_x, cons.img_y),
+#             allow_stretch=True,
+#         )
+#         self.add_widget(self.label)
+
+#         self.setNewPositionGoal()
+
+#         # 애니메이션, 상태, 이동 타이머 시작
+#         self.timer_state_controler_ev = Clock.schedule_interval(self.state_controler, cons.state_control_loop_interval)
+
+#         # 키 입력 리스너 추가
+#         Window.bind(on_key_down=self._on_key_down)
+
+#     def all_timer_stop(self):
+#         if self.timer_idle_ev:
+#             self.timer_idle_ev.cancel()
+#             self.timer_idle_ev = None
+#         if self.timer_run_ev:
+#             self.timer_run_ev.cancel()
+#             self.timer_run_ev = None
+#         if self.timer_moving_ev:
+#             self.timer_moving_ev.cancel()
+#             self.timer_moving_ev = None
+
+#     def state_controler(self, dt):
+#         self.all_timer_stop()
+#         if self.state == 0:  # 제자리
+#             self.img_idle_state = random.randint(0, len(cons.idle_list)-1)
+#             self.timer_idle_ev = Clock.schedule_interval(self.idle_anim, cons.idle_motion_loop_interval)
+#             self.state = 1
+#         elif self.state == 1:  # 이동+달리기
+#             self.setNewPositionGoal()
+#             self.timer_run_ev = Clock.schedule_interval(self.run_anim, cons.run_motion_loop_interval)
+#             self.timer_moving_ev = Clock.schedule_interval(self.move_window, cons.window_move_loop_interval)
+#             self.state = 0
+
+#     def idle_anim(self, dt):
+#         self.label.source = resource_path(cons.idle_list[self.img_idle_state])
+
+#     def run_anim(self, dt):
+#         # 두 이미지를 번갈아가며 사용
+#         if self.img_run_state == 0:
+#             path = resource_path(cons.run_motion1_path)
+#             self.img_run_state = 1
+#         else:
+#             path = resource_path(cons.run_motion2_path)
+#             self.img_run_state = 0
+#         # 좌우 반전(수동. Kivy는 horizontal flip 기본 지원이 없어 이미지 전처리 필요)
+#         self.label.source = path
+#         # Kivy에서 실시간으로 flip하려면, 별도 커스텀 이미지 처리 필요
+
+#     def move_window(self, dt):
+#         temp_x = self.now_x
+#         temp_y = self.now_y
+#         if self.now_x < self.goal_x:
+#             self.now_x += self.speed
+#             self.img_run_direction = -1
+#         elif self.now_x > self.goal_x:
+#             self.now_x -= self.speed
+#             self.img_run_direction = 1
+
+#         if self.now_y < self.goal_y:
+#             self.now_y += self.speed
+#         elif self.now_y > self.goal_y:
+#             self.now_y -= self.speed
+
+#         # 실제 윈도우 창 위치 이동
+#         Window.left = self.now_x
+#         Window.top = self.now_y
+
+#         if temp_x == self.now_x and temp_y == self.now_y:
+#             self.setNewPositionGoal()
+
+#     def setNewPositionGoal(self):
+#         self.goal_x = random.randint(0, self.max_x - cons.window_size_x)
+#         self.goal_x -= self.goal_x % self.speed
+#         self.goal_y = random.randint(0, self.max_y - cons.window_size_y)
+#         self.goal_y -= self.goal_y % self.speed
+
+#     def _on_key_down(self, window, key, scancode, codepoint, modifier):
+#         # 'q' key 종료
+#         if codepoint.lower() == 'q':
+#             App.get_running_app().stop()
+
+# class TransparentWindowApp(App):
+#     def build(self):
+#         # 윈도우 창 설정: 완전 투명 & 프레임 없음
+#         Window.size = (cons.window_size_x, cons.window_size_y)
+#         Window.left = cons.window_x
+#         Window.top = cons.window_y
+#         Window.clearcolor = (0, 0, 0, 0)  # 완전 투명 배경
+#         Window.borderless = True  # 프레임/테두리 제거
+#         return CharacterWidget()
+
+# if __name__ == "__main__":
+#     TransparentWindowApp().run()
